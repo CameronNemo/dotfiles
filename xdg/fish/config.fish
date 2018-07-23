521002e0377687b91c10d972a11be06eea30b994
Cameron Nemo <camerontnorman@gmail.com>
2018-07-23 13:41:51 -0700

    sway: use new elementary Terminal path; fish: use non-conflicting aliases for xbps commands

--- xdg/fish/config.fish
+++ xdg/fish/config.fish
@@ -1,8 +1,8 @@
 set -u fish_greeting
 
-alias xi='sudo xbps-install'
-alias xr='sudo xbps-remove'
-alias xu='sudo xbps-install -Suv'
+alias xin='sudo xbps-install'
+alias xrm='sudo xbps-remove'
+alias xup='sudo xbps-install -Suv'
 
 if test "$XDG_VTNR" = "1"; and test -z "$DISPLAY"; and test -z "$WAYLAND_DISPLAY";
 	export DESKTOP_SESSION="sway"
